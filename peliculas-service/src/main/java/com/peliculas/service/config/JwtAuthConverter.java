4980befc9c2aa40c60f6ab1e5f9ea56d15eb7128
Diego Farid Solorzano Villegas <PT72787033@idat.pe>
2026-07-19 15:31:47 -0500

    Se integró Keycloack y Spring Security con jwt. También se realizó pruebas de control de acceso por roles en peliculas-service con éxito.

---
+++ peliculas-service/src/main/java/com/peliculas/service/config/JwtAuthConverter.java
@@ -0,0 +1,31 @@
+package com.peliculas.service.config;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken>{
+	@Override
+	public AbstractAuthenticationToken convert(Jwt jwt) {
+		Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+		Collection<SimpleGrantedAuthority> authorities = List.of();
+		if(realmAccess != null && realmAccess.containsKey("roles")) {
+			Object rolesObject = realmAccess.get("roles");
+			if(rolesObject instanceof List<?> rolesList) {
+				authorities = rolesList.stream()
+						.map(Object::toString)
+						.map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+						.collect(Collectors.toList());
+			}
+		}
+		return new JwtAuthenticationToken(jwt, authorities);
+	}
+
+}
---
+++ peliculas-service/src/main/java/com/peliculas/service/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.peliculas.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth -> auth
+
+                        // Swagger
+                        .requestMatchers("/swagger-ui/**","/v3/api-docs/**","/swagger-ui.html").permitAll()
+
+                        // Consultar películas
+                        .requestMatchers(HttpMethod.GET, "/v1/peliculas/**").hasAnyRole("USER", "ADMIN")
+
+                        // Registrar películas
+                        .requestMatchers(HttpMethod.POST, "/v1/peliculas/**").hasRole("ADMIN")
+
+                        // Actualizar películas
+                        .requestMatchers(HttpMethod.PUT, "/v1/peliculas/**").hasRole("ADMIN")
+
+                        // Eliminar películas
+                        .requestMatchers(HttpMethod.DELETE, "/v1/peliculas/**").hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated()
+                )
+
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwt->
+                        		jwt.jwtAuthenticationConverter(new JwtAuthConverter())
+                        		)
+                        );
+
+        return http.build();
+    }
+}
